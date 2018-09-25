68831346b97406114dbef755f43e2ec952dc7b67
Oasiris <teracontinens@gmail.com>
2018-09-25 01:50:53 -0400

    Header + background progress

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import Header from './Header'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="app-outer">
         <Header />
       
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './../css/header.css'
 
+import headerDiamond from './../svg/header-diamond.svg';
+
 export default class Header extends Component {
   render() {
     return (
@@ -8,7 +10,9 @@ export default class Header extends Component {
         <h1 id="main-title">david hong</h1>
         <div id="header-desc">
           <span id="developer-desc" className="header-desc">developer</span>
-          <span className="header-diamond"></span>
+          <span className="header-diamond">
+            <img src={headerDiamond} alt="" id="header-diamond" />
+          </span>
           <span id="designer-desc" className="header-desc">designer</span>
         </div>
       </div>
